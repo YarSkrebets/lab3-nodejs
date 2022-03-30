2d019c131add3c942f1b824ef94bc35a88737819
Yaroslav Skrebets <60351897+YarSkrebets@users.noreply.github.com>
2022-03-31 00:55:52 +0300

    Update favourites.js

--- routes/favourites.js
+++ routes/favourites.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "mysql",
     port: 3306,
     database: 'test',
     user: "root",
